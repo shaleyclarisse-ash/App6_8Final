f7c1184f4b24324ee99aebfe7dff4ecc78dfc136
Ashley <shaleyclarisse@gmail.com>
2026-02-23 23:16:08 +0800

    commit

---
+++ app/src/main/java/com/example/appact4_5/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.appact4_5;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+    }
+}
